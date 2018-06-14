ee0042c90c9cd4e1d36b66f02e94f8fe0bf546c4
XWxiaowei <2809641033@qq.com>
2018-06-14 23:00:39 +0800

    第5课，模板引擎 Thymeleaf

---
+++ 第05课：Jpa 和 Thymeleaf 实践/spring-boot-jpa-thymeleaf/src/main/java/com/jay/spring/web/IndexController.java
@@ -0,0 +1,21 @@
+package com.jay.spring.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by xiang.wei on 2018/6/14
+ *
+ * @author xiang.wei
+ */
+@Controller
+public class IndexController {
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+    @RequestMapping("/home")
+    public String home() {
+        return "layout/home";
+    }
+}
